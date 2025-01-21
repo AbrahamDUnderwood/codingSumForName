d72c4325614cbf595db1629cf5a68c745e9a88c0
Abraham D. Underwood <abrahamdunderwood@tutanota.com>
2025-01-21 12:24:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function calculateNameSum(name) {
+  let sum = 0;
+  name = name.toUpperCase();
+
+  for (let i = 0; i < name.length; i++) {
+    let charCode = name.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      sum += charCode - 65 + 1;
+    }
+  }
+  return sum;
+}
+
+function getUserInputAndCalculateSum() {
+  let name = prompt('Please enter a name:');
+  let sum = calculateNameSum(name);
+
+  alert(`The sum of the name "${name}" is: ${sum}`);
+
+  //   other possible option do display the sum
+  //   console.log(`The sum of the name "${name}" is: ${sum}`);
+}
+
+getUserInputAndCalculateSum();
